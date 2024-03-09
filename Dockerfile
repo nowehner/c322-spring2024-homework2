f8a3665c2825526f2eed22c339a0afd94ef27365
nowehner <97761745+nowehner@users.noreply.github.com>
2024-03-08 20:23:50 -0600

    dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM eclipse-temurin:17
+
 WORKDIR /home
-COPY ./target/lab6-0.0.1.jar lab6.jar
-ENTRYPOINT ["java", "-jar", "lab6.jar"]
+
+COPY ./target/guitar-inventory-management-0.0.1-SNAPSHOT.jar guitar-inventory-management.jar
+
+ENTRYPOINT ["java", "-jar", "guitar-inventory-management.jar"]
--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,4 +1,17 @@
 package controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin
 public class HomeController {
+    @GetMapping("/")
+    public String greetings(){
+        return "Welcome to the inventory management service";
+    }
+
+
 }

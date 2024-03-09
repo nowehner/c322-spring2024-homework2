5a9b65142a158cb43b5ec09e71330e5b3b759c35
nowehner <97761745+nowehner@users.noreply.github.com>
2024-03-08 22:13:02 -0600

    homework 6

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin
+
 public class HomeController {
     @GetMapping("/")
     public String greetings(){
